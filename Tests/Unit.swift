70e49b481cb41293539b6a4acdfbe24301da4f32
windmill <windmill@greece.local>
2018-02-13 09:59:14 +1100

    Initial Commit

---
+++ Tests/Unit.swift
@@ -0,0 +1,25 @@
+//
+//  Unit.swift
+//  HelloWindmillTests
+//
+//  Created by windmill on 13/02/2018.
+//  Copyright © 2018 windmill. All rights reserved.
+//
+
+import XCTest
+@testable import HelloWindmill
+
+class Unit: XCTestCase {
+    
+    func testSuccess() {
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            for i in 1...100 {
+                print(i)
+            }
+        }
+    }
+}
